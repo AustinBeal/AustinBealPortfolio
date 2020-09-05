6d33ef751c02373a277446c459cf140dd27001cb
AustinBeal <austinbeal89@gmail.com>
2020-09-05 17:45:56 -0500

    added pages and made media queries for base

---
+++ javamain.js
@@ -0,0 +1,24 @@
+function toggleBar() {
+  document.getElementById("topnav").classList.toggle('show');
+  document.getElementById("hamburger").classList.toggle('active');  
+}
+
+function openWinGit() {
+  myWindow = window.open("https://github.com/AustinBeal");   // Opens a new window
+}
+
+function openWinCode() {
+  myWindow = window.open("https://codepen.io/AustinBeal");   // Opens a new window
+}
+
+function openWinEtsy() {
+  myWindow = window.open("https://www.etsy.com/shop/austinsummerstudio");   // Opens a new window
+}
+
+function openWinLink() {
+  myWindow = window.open("https://www.linkedin.com/in/austin-beal/");   // Opens a new window
+}
+
+function openWinInsta() {
+  myWindow = window.open("https://www.instagram.com/austinsummerstudio/");   // Opens a new window
+}
